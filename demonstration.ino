27fcd6653d2c111a2fd05858bc034b600cb88bd6
nemihail <nemihail02@gmail.com>
2025-10-30 11:35:26 +0700

    Add files via upload
    новая КРУТААААЯ вотермарка уникума и пинмоды концевиков

--- demonstration.ino
+++ demonstration.ino
@@ -7,6 +7,21 @@
 #define right_motor_forward 9
 #define right_motor_back 6
 
+#define down_limit_pin A7
+#define up_limit_pin A6
+
+
+/*
+ *     _    _                          _    _
+ *    \ \  / /                        \ \  / /
+ *     \ \/ / _   _   _   __   _   __  \ \/ /  _    _
+ *      \  / | | | | | | /  | | | / /   \  /  | \  / |
+ *      / /  | |_| | | |/   | | |/ /    / /   |  \/  |
+ *     / /   |  _  | |   /| | | |\ \   / /    | |\/| |
+ *    /_/    |_| |_| |__/ |_| |_| \_\ /_/     |_|  |_|
+ */
+
+
 void setup() {
   pinMode(left_motor_forward, OUTPUT);
   pinMode(left_motor_back, OUTPUT);
@@ -15,6 +30,7 @@ void setup() {
 }
 
 void loop() {
+  wheels();
 }
 
 void wheels(){
